2f21092960fb90e6e9298bf84516da00f316e565
Fernando Jimenez <fgjv06@gmail.com>
2023-03-30 05:53:18 -0300

    JS - getting all required elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// Getting all required elements
+const startBtn = document.querySelector(".start-btn button");
+const infoBox = document.querySelector(".info-box");
+const exitBtn = infoBox.querySelector(".buttons .quit");
+const continueBtn = document.querySelector(".buttons .restart");
