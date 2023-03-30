ac1fbbf5c1cb401229c96cd7b33f55350bc6612e
Fernando Jimenez <fgjv06@gmail.com>
2023-03-30 06:13:28 -0300

    JS - function for when start button clicked

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 const startBtn = document.querySelector(".start-btn button");
 const infoBox = document.querySelector(".info-box");
 const exitBtn = infoBox.querySelector(".buttons .quit");
-const continueBtn = document.querySelector(".buttons .restart");
+const continueBtn = infoBox.querySelector(".buttons .restart");
+
+// When start button clicked
+startBtn.onclick = () => {
+  infoBox.classList.add("activeInfo");
+}
